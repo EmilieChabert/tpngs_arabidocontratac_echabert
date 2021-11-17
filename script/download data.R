8cc329b95fee31ca53a0c38cbd2a76781d22563d
EmilieChabert <emilie.chabert@ens-lyon.fr>
2021-11-17 15:52:44 +0000

    Download ATAC-seq data (INTACT, all root + published data) + genome reference and annotations

---
+++ script/download data.R
@@ -0,0 +1,23 @@
+#download INTACT ATAC-seq data
+wget --user='tp_ngs' --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/Data/2019_006_S6_R1.fastq.gz
+wget --user='tp_ngs' --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/Data/2019_006_S6_R2.fastq.gz
+
+#download ATAC-seq data all root
+wget --user='tp_ngs' --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/Data/2020_378_S8_R1.fastq.gz
+wget --user='tp_ngs' --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/Data/2020_378_S8_R2.fastq.gz
+
+#download publicly available ATAC-seq data
+#GSM2260235: Col-0_50k_root_nuclei_FANS-ATAC-seq_rep1; Arabidopsis thaliana; OTHER(SRR4000472)
+fastq-dump --split-files SRR4000472
+
+#download reference genome and annotations from  ENSEMBL database
+wget http://ftp.ebi.ac.uk/ensemblgenomes/pub/release-51/plants/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa.gz
+wget http://ftp.ebi.ac.uk/ensemblgenomes/pub/release-51/plants/gtf/arabidopsis_thaliana/Arabidopsis_thaliana.TAIR10.51.gtf.gz
+
+# Dézipper
+gunzip A_thaliana_genome/Arabidopsis_thaliana.TAIR10.51.gtf.gz
+gunzip A_thaliana_genome/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa.gz
+gunzip data/*
+
+#download regions of the genome that can induce a bias in the analysis
+wget --user='tp_ngs' --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/Supporting_files/TAIR10_selectedRegions.bed
--- script/download.R
+++
@@ -1,7 +0,0 @@
-#download INTACT ATAC-seq data
-wget --user='tp_ngs' --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/Data/
-
-#download publicly available ATAC-seq data
-#GSM2260235: Col-0_50k_root_nuclei_FANS-ATAC-seq_rep1; Arabidopsis thaliana; OTHER(SRR4000472)
-fastq-dump SRR4000472
-
