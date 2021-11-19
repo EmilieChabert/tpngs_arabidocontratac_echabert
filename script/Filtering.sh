caf1adf968991f782d7f11058a22df7e8f05089b
EmilieChabert <emilie.chabert@ens-lyon.fr>
2021-11-19 15:47:15 +0000

    filtrage qualité du mapping, reads dupliques, genome Mt et Pt

--- script/Filtering.sh
+++ script/Filtering.sh
@@ -15,11 +15,25 @@ do
       
 done
 
-#for mapfile in processed_data/mapping/*bam
-#do
 
-#samtools view -b
+# Enlever le genome mitochondrial et chloroplastique
 
-#done
+grep -v -E "Mt|Pt" A_thaliana_genome/TAIR10_selectedRegions.bed > A_thaliana_genome/TAIR10_selectedRegions_nuc.bed
+#sélectionne les deux lignes contenant termes/ -v tout sauf ça
 
-#-b --> output in the BAM format
+
+#Filtrer
+for mapfile in processed_data/mapping/*duplicates.bam
+
+do
+  samtools view -b ${mapfile} -o ${mapfile/".bam"/"filtered.bam"} \
+  -L A_thaliana_genome/TAIR10_selectedRegions_nuc.bed \
+  -F 1024 -f 3 -q 30
+  
+      
+done
+
+# -L A_thaliana_genome/TAIR10_selectedRegions_nuc.bed --> enlever le genome mitochondrial et chloroplastique
+# -F 1024 --> enleve les reads dupliques
+# -f 3 --> garde les read paired et  read mapped in proper pair
+# -q 30 --> reads de qualite MAPQ > 30
