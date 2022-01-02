ae5b808e8585560bdbc353bf0e286b7a6a129028
EmilieChabert <emilie.chabert@ens-lyon.fr>
2022-01-02 10:10:25 +0000

    annotations + modification de la  workingDir

--- script/Quality_check.sh
+++ script/Quality_check.sh
@@ -1,9 +1,25 @@
+#!/bin/bash
+#
+# Analyse de la qualité des reads
+# 
+
+workingDir=/home/rstudio/mydatalocal/tpngs_arabidocontratac_echabert
+
+#////////////////////// Start of the script
 #Analysis of the read quality
+mkdir ${workingDir}/processed_data/fastqc
+
 fastqc data/*
-mv ./data/*fastqc* fastqc
-mv ./fastqc processed data
+#nombreux graphiques donnant la qualité des bases en fonction de la position dans le reads,
+#qualite moyenne des reads,
+#contenu GC, nombre de séquences dupliquées, contenu en adaptateurs ...
+
+mv ${workingDir}/data/*fastqc* ${workingDir}/processed_data/fastqc
 
 # Compile all the fastqc analysis
+mkdir ${workingDir}/processed_data//multiqc_data
+
 multiqc fastqc/*
-mv ./multiqc_report.html multiqc_data
-mv ./multiqc_data processed data
+mv ${workingDir}/multiqc_report.html ${workingDir}/processed_data//multiqc_data
+
+# End of the script \\\\\\\\\\\\\\\\\\\\\\\\\\\\
